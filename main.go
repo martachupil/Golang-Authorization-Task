c49c751fea431883dbb8592a2c1dad5377260aac
Marta <mchupil@mail.ru>
2017-11-26 23:13:46 +0200

    main page init

--- main.go
+++ main.go
@@ -1,13 +1,37 @@
 package main
 
 import ("fmt"
-		"net/http")
+		"html/template"
+		"net/http"
+		"path"
+)
 
-func main() {
-	http.HandleFunc("/", index_handler)
-	http.ListenAndServe(":8000", nil)
+type Profile struct {
+	Name    string
+	Hobbies []string
 }
 
 func index_handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "My name is Angel :)")
+	profile := Profile{"Marta", []string{"sport", "programming"}}
+
+	fp := path.Join("templates", "index.html")
+	tmpl, err := template.ParseFiles(fp)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	if err := tmpl.Execute(w, profile); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func about_handler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprintf(w, "This site is about nothing")
+}
+
+func main() {
+	http.HandleFunc("/", index_handler)
+	http.HandleFunc("/about/", about_handler)
+	http.ListenAndServe(":8000", nil)
 }
