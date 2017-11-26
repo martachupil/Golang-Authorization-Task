fedb2d8ff211d00ddab865809058df686ef16c09
Marta <mchupil@mail.ru>
2017-11-27 01:54:05 +0200

    sign up changes

--- main.go
+++ main.go
@@ -1,10 +1,18 @@
 package main
 
-import ("fmt"
-		"html/template"
-		"net/http"
-		"path"
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+	"path"
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
+	"golang.org/x/crypto/bcrypt"
 )
+
+var db *sql.DB
+var err error
 
 type Profile struct {
 	Name    string
@@ -49,8 +57,92 @@ func errorHandler(w http.ResponseWriter, r *http.Request, status int) {
 	}
 }
 
+func loginPage(res http.ResponseWriter, req *http.Request) {
+	if req.Method != "POST" {
+		http.ServeFile(res, req, "templates/login.html")
+		return
+	}
+
+	username := req.FormValue("username")
+	password := req.FormValue("password")
+
+	var databaseUsername string
+	var databasePassword string
+
+	err := db.QueryRow("SELECT username, password FROM users WHERE username=?", username).Scan(&databaseUsername, &databasePassword)
+
+	if err != nil {
+		http.Redirect(res, req, "/login", 301)
+		return
+	}
+
+	err = bcrypt.CompareHashAndPassword([]byte(databasePassword), []byte(password))
+	if err != nil {
+		http.Redirect(res, req, "/login", 301)
+		return
+	}
+
+	res.Write([]byte("Hello" + databaseUsername))
+
+}
+
+func signupPage(res http.ResponseWriter, req *http.Request) {
+	if req.Method != "POST" {
+		http.ServeFile(res, req, "templates/signup.html")
+		return
+	}
+
+	username := req.FormValue("username")
+	password := req.FormValue("password")
+
+	var user string
+
+	err := db.QueryRow("SELECT username FROM users WHERE username=?", username).Scan(&user)
+
+	switch {
+	case err == sql.ErrNoRows:
+		hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+		if err != nil {
+			http.Error(res, "Server error, unable to create your account.", 500)
+			return
+		}
+
+		_, err = db.Exec("INSERT INTO users(username, password) VALUES(?, ?)", username, hashedPassword)
+		if err != nil {
+			http.Error(res, "Server error, unable to create your account.", 500)
+			return
+		}
+
+		res.Write([]byte("User created!"))
+		return
+	case err != nil:
+		http.Error(res, "Server error, unable to create your account.", 500)
+		return
+	default:
+		http.Redirect(res, req, "/", 301)
+	}
+}
+
 func main() {
+
+	// we create here an sql.DB and checking for errors
+	db, err = sql.Open("mysql", "root:root@/mysql")
+	if err != nil {
+		panic(err.Error())
+	}
+	// when func ends => close it
+	defer db.Close()
+
+	// Test the connection to the database
+	err = db.Ping()
+	if err != nil {
+		panic(err.Error())
+	}
+
+
 	http.HandleFunc("/", index_handler)
 	http.HandleFunc("/about/", about_handler)
+	http.HandleFunc("/signup", signupPage)
+	http.HandleFunc("/login", loginPage)
 	http.ListenAndServe(":8000", nil)
 }
