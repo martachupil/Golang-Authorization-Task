40bd061c27225d2853d4a08af5c35519807aa92c
Marta <mchupil@mail.ru>
2017-11-26 23:38:46 +0200

    404 page added

--- main.go
+++ main.go
@@ -16,6 +16,12 @@ func index_handler(w http.ResponseWriter, r *http.Request) {
 
 	fp := path.Join("templates", "index.html")
 	tmpl, err := template.ParseFiles(fp)
+
+	if r.URL.Path != "/" {
+		errorHandler(w, r, http.StatusNotFound)
+		return
+	}
+
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
@@ -26,8 +32,21 @@ func index_handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func about_handler(w http.ResponseWriter, r *http.Request) {
+
+	if r.URL.Path != "/about/" {
+		errorHandler(w, r, http.StatusNotFound)
+		return
+	}
+
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	fmt.Fprintf(w, "This site is about nothing")
+}
+
+func errorHandler(w http.ResponseWriter, r *http.Request, status int) {
+	w.WriteHeader(status)
+	if status == http.StatusNotFound {
+		fmt.Fprint(w, "custom 404")
+	}
 }
 
 func main() {
